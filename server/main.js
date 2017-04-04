1620b4d82b28a496dd5756dfbeadfb7a50716c87
calamc <cmmacmac@gmail.com>
2017-04-04 21:21:34 +0100

    Add more user system functionality

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// import users validation simpleschema
+import '../imports/api/users';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
